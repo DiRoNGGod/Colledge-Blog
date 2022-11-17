a462403bb82ccec858d55f7641b37d8d7ec21018
DiRoNG <>
2022-11-17 09:46:53 +0300

    Сделано появление навигации при нажатии на кнопку

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 "use strict"
+
+// & Создание выплывающего меню
+
+const menuBtn = document.querySelector(".menu-btn");
+const headerNav = document.querySelector(".header__nav");
+
+menuBtn.addEventListener('click', function(e) {
+    headerNav.classList.toggle('_active');
+    document.querySelector(".menu-btn__content").classList.toggle('_active');
+})
